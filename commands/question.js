6e4d1f1a2c1e49e05d6c242a755b62cb59a0eec3
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-10-10 11:13:53 -0400

    Clean-up maybe?

--- commands/question.js
+++ commands/question.js
@@ -6,58 +6,54 @@ module.exports = {
 		console.log(message.author.username + " in " + message.guild.name);
     const Discord = require('discord.js');
     const q = Math.floor(Math.random() * 4);
+		const one = Math.floor(Math.random() * 51);
+		const two = Math.floor(Math.random() * 51);
+		const onea = one-(one*2)
+		const twoa = two-(two*2)
     if (q == 0) {
-      const one = Math.floor(Math.random() * 51);
-      const two = Math.floor(Math.random() * 51);
       const answer = one+two;
   		message.channel.send(one + " + " + two);
           const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { max: 1});
           collector.on('collect', message => {
               if (message.content == answer) {
                   message.channel.send("💎Correct! 💎");
+									console.log(message.author.username + " in " + message.guild.name + " got an addition question correct. " + answer);
               } else  {
                   message.channel.send("Wrong.");
               }
           })
     } else if (q == 1) {
-      const one = Math.floor(Math.random() * 51);
-      const two = Math.floor(Math.random() * 51);
       const answer = one-two;
   		message.channel.send(one + " - " + two);
           const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { max: 1});
           collector.on('collect', message => {
               if (message.content == answer) {
                   message.channel.send("💎Correct! 💎");
+									console.log(message.author.username + " in " + message.guild.name + " got a subtraction question correct. " + answer);
               } else  {
                   message.channel.send("Wrong.");
               }
           })
     } else if (q == 2) {
-      const one = Math.floor(Math.random() * 51);
-      const onea = one-(one*2)
-      const two = Math.floor(Math.random() * 51);
-      const twoa = two-(two*2)
       const answer = (onea-twoa);
       message.channel.send(onea + " - " + twoa);
           const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { max: 1});
           collector.on('collect', message => {
               if (message.content == answer) {
                   message.channel.send("💎Correct! 💎");
+									console.log(message.author.username + " in " + message.guild.name + " got a negative subtraction question correct. " + answer);
               } else  {
                   message.channel.send("Wrong.");
               }
           })
     } else if (q == 3) {
-			const one = Math.floor(Math.random() * 51);
-			const onea = one-(one*2)
-			const two = Math.floor(Math.random() * 51);
-			const twoa = two-(two*2)
 			const answer = (onea+twoa);
 			message.channel.send(onea + " + " + twoa);
 					const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { max: 1});
 					collector.on('collect', message => {
 							if (message.content == answer) {
 									message.channel.send("💎Correct! 💎");
+									console.log(message.author.username + " in " + message.guild.name + "got a negative addition question correct." + answer);
 							} else  {
 									message.channel.send("Wrong.");
 							}
