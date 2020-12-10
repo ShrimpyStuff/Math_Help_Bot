05515d527a1a6beb8ff5fd6df09132b8abe64714
ElCholoGamer <josedanielgrayson@gmail.com>
2020-12-09 21:59:02 -0500

    Some formatting I think

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const {prefix, token, config } = require('./config.json');
+const { prefix, token, config } = require('./config.json');
 client.commands = new Discord.Collection();
-require("dotenv").config();
+require('dotenv').config();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+	.readdirSync('./commands')
+	.filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
+client.once('ready', () => console.log('Ready!'));
 
 client.on('message', message => {
+	if (!message.content.startsWith(prefix)) return;
 
-if (!message.content.startsWith(prefix)) return;
-
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const args = message.content.slice(prefix.length).trim().split(/\s+/);
 	const commandName = args.shift().toLowerCase();
 
-if (message.content === prefix + "invite") {message.channel.send("https://discord.com/api/oauth2/authorize?client_id=764271962087751731&permissions=346176&scope=bot");}
+	if (message.content === prefix + 'invite') {
+		message.channel.send(
+			'https://discord.com/api/oauth2/authorize?client_id=764271962087751731&permissions=346176&scope=bot'
+		);
+	}
 
-const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-if (!command) return;
+	const command =
+		client.commands.get(commandName) ||
+		client.commands.find(
+			cmd => cmd.aliases && cmd.aliases.includes(commandName)
+		);
+	if (!command) return;
 
-try {
-	command.execute(message, args);
-} catch (error) {
-	console.error(error);
-	message.reply('there was an error trying to execute that command!');
-}
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
 });
 
 client.login(process.env.BOT_TOKEN);
